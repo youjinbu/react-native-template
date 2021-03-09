579fad9db578d96f6f2d1448513979186f4d4c77
hehe <xpc1993@gmail.com>
2021-03-09 12:37:33 +0800

    chore: ios 11 & enable inlineRequires

--- template/metro.config.js
+++ template/metro.config.js
@@ -4,7 +4,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
